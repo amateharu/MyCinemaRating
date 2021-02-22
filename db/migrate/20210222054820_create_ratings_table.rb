d22c14a1be092b460ee3bfb4e51ea397a87cda10
Oleksii Riabukha <Oleksii_Riabukha@epam.com>
2021-02-22 13:16:18 +0200

    Fix tables

--- db/migrate/20210222054820_create_ratings_table.rb
+++ db/migrate/20210222054820_create_ratings_table.rb
@@ -1,6 +1,6 @@
 class CreateRatingsTable < ActiveRecord::Migration[5.2]
   def change
-    create_table :ratings_tables do |t|
+    create_table :ratings do |t|
       t.integer :user_id
       t.integer :film_id
       t.integer :user_rating
--- db/schema.rb
+++ db/schema.rb
@@ -70,19 +70,4 @@ ActiveRecord::Schema.define(version: 2021_02_22_054820) do
     t.integer "user_id"
   end
 
-  create_table "ratings_tables", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "film_id"
-    t.integer "user_rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
